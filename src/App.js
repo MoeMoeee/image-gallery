1d2a1bbd5100bb7ddc810e77a20d685fd50aba04
MoeMoeee <dangkhoa.140502@gmail.com>
2023-01-29 18:44:11 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import ImageCard from "./components/imageCard";
+import ImageSearch from "./components/imageSearch";
 
 function App() {
   const [images, setImages] = useState([]);
@@ -20,15 +21,19 @@ function App() {
       setIsLoading(false)
     })
     .catch(err => console.log(err))
-  }, []);
+  }, [term]);
 
   return (
     <div className="container mx-auto">
-        <div className="grid grid-cols-3 gap-4">
-            {images.map(image => (
-                <ImageCard key={image.id} image={image} />
-            ))}
-        </div>
+      <ImageSearch searchText={(text) => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && <h1 className="text-5xl text-center mx-auto mt-32">No Images Found</h1> }
+
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> : <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
     </div>
   );
 }
